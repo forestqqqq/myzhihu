637e49eeae2e0bd3fb49f0e907ede264a54ce7a1
zhanghaiwei <forestqqqq@163.com>
2016-04-20 16:38:58 +0800

    初始化项目

---
+++ src/main/java/com/forest/myzhihu/JFinalMyZhihuConfig.java
@@ -0,0 +1,35 @@
+package com.forest.myzhihu;
+
+import com.jfinal.config.*;
+
+/**
+ * @author zhanghaiwei
+ * @version 1.0
+ * @date 16/4/20 下午4:32
+ */
+public class JFinalMyZhihuConfig extends JFinalConfig {
+	@Override
+	public void configConstant(Constants constants) {
+
+	}
+
+	@Override
+	public void configRoute(Routes routes) {
+
+	}
+
+	@Override
+	public void configPlugin(Plugins plugins) {
+
+	}
+
+	@Override
+	public void configInterceptor(Interceptors interceptors) {
+
+	}
+
+	@Override
+	public void configHandler(Handlers handlers) {
+
+	}
+}
