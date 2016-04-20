e5fec21d5b96932c6e23e5aac0fbf8f8a39d71df
zhanghaiwei <forestqqqq@163.com>
2016-04-20 16:51:09 +0800

    完成第一个版本,只能访问首页，首页只输出一个测试文本

--- src/main/java/com/forest/myzhihu/JFinalMyZhihuConfig.java
+++ src/main/java/com/forest/myzhihu/JFinalMyZhihuConfig.java
@@ -1,5 +1,6 @@
 package com.forest.myzhihu;
 
+import com.forest.myzhihu.index.IndexController;
 import com.jfinal.config.*;
 
 /**
@@ -10,12 +11,12 @@ import com.jfinal.config.*;
 public class JFinalMyZhihuConfig extends JFinalConfig {
 	@Override
 	public void configConstant(Constants constants) {
-
+		constants.setDevMode(true);
 	}
 
 	@Override
 	public void configRoute(Routes routes) {
-
+		routes.add("/", IndexController.class);
 	}
 
 	@Override
---
+++ src/main/java/com/forest/myzhihu/index/IndexController.java
@@ -0,0 +1,16 @@
+package com.forest.myzhihu.index;
+
+import com.jfinal.core.Controller;
+
+/**
+ * @author zhanghaiwei
+ * @version 1.0
+ * @date 16/4/20 下午4:44
+ */
+public class IndexController extends Controller{
+
+	public void index() throws Exception{
+		renderText("测试首页");
+	}
+
+}
